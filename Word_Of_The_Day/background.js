f749bdc182902c159b71525ff987aa45a6004659
Arjun Reddy <mail.arjunreddy001@gmail.com>
2023-07-02 23:10:29 +0530

    Corrections & Imporvements

--- Word_Of_The_Day/background.js
+++ Word_Of_The_Day/background.js
@@ -1,6 +1,20 @@
 // Listen for messages
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if (msg.name === "FETCH_WORDS") {
+    // https://api.quotable.io/random
+    /*
+        {
+            "id": "",
+            "content": "",
+            "author": "",
+            "authorSlug": "",
+            "tags": ["", ""],
+            "dateAdded": "",
+            "dateModified": "",
+            "length": 76
+        }
+    */
+
     const apiUrl = "https://api.quotable.io/random";
 
     fetch(apiUrl)
--- Word_Of_The_Day/newtab.js
+++ Word_Of_The_Day/newtab.js
@@ -1,23 +1,7 @@
-// https://api.quotable.io/random
-/*
-    {
-        "id": "",
-        "content": "",
-        "author": "",
-        "authorSlug": "",
-        "tags": ["", ""],
-        "dateAdded": "",
-        "dateModified": "",
-        "length": 76
-    }
-*/
-
 // Send message to background
-chrome.runtime.sendMessage({ name: "FETCH_WORDS" }, async (res) => {
-  // Wait for response
-  console.log(res);
-
-  // Update display on content script
+chrome.runtime.sendMessage({ name: "FETCH_WORDS" }, (res) => {
+  // After recieving response
+  // Update content in newtab.html
   document.querySelector("h1").innerHTML = res.data.author;
   document.querySelector("p").innerHTML = res.data.content;
 });
